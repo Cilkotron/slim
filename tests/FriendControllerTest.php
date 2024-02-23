3ccfa4b9a862b8b7d1666e32f769371e5b9129b4
Sanja Budic <sanjabudic@Sanjas-MacBook-Air.local>
2024-02-23 12:27:43 +0100

    Adding setUpContainer mmethod

--- tests/FriendControllerTest.php
+++ tests/FriendControllerTest.php
@@ -4,6 +4,8 @@ use App\Controller\FriendController;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Slim\Psr7\Factory\ResponseFactory;;
 use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+use UnexpectedValueException;
 
 require_once __DIR__ . '/../src/FriendController.php';
 
@@ -11,8 +13,8 @@ require_once __DIR__ . '/../src/FriendController.php';
 
 class FriendControllerTest extends \PHPUnit\Framework\TestCase
 {
-
     protected $controller;
+    protected $container;
 
     protected function setUp(): void
     {
@@ -25,14 +27,24 @@ class FriendControllerTest extends \PHPUnit\Framework\TestCase
             ->addDefinitions(__DIR__ . '/../config/definitions.php')
             ->build();
         $this->controller = $container->get(FriendController::class);
-        var_dump($this->controller);
+        $this->setUpContainer($container);
 
     }
+    protected function setUpContainer(ContainerInterface $container = null): void
+    {
+        if ($container instanceof ContainerInterface) {
+            $this->container = $container;
+
+            return;
+        }
+
+        throw new UnexpectedValueException('Container must be initialized');
+    }
+
     public function testGetAllFriends()
     {
         $request = (new ServerRequestFactory())->createServerRequest('GET', '/friends');
         $response = (new ResponseFactory())->createResponse();
-
         $result = $this->controller->getAllFriends($request, $response, []);
         $this->assertSame(200, $result->getStatusCode());
     }
